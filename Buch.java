9747083adf30dc12d45cbb5b83729765a0e3f022
jaenefab <jaenefab@students.zhaw.ch>
2017-09-18 13:59:08 +0200

    corrected

--- Buch.java
+++ Buch.java
@@ -6,17 +6,17 @@
  */
 public class Buch {
 	public String titel;
-	private int isbn;
+	private String isbn;
 	// Der aktuelle Bestand
-	private int b;
+	private int bestand;
 	
 	/**
 	 * Erzeugt ein Buch mit den spezifizierten Werten fuer 
 	 * Titel und ISBN-Nummer und setzt den Bestand auf 0.
 	 * @param buchTitel Der Buchtitel.
 	 */
-	public Buch(String buchTitel) {
-		titel = buchTitel
+	public Buch(String buchTitel, String buchISBN) {
+		titel = buchTitel;
 		isbn = buchISBN;
 	}
 	
@@ -25,7 +25,7 @@ public class Buch {
 	 * @return Der Bestand.
 	 */
 	public int bestand() {
-		return b;
+		return bestand;
 	}
 	
 	/**
@@ -33,16 +33,15 @@ public class Buch {
 	 * @param veraenderung Die Veraenderung des Bestands.
 	 */
 	public void veraendereBestand(int veraenderung) {
-		b = b + veraenderung;
-		return b;
+		bestand = bestand + veraenderung;
 	}
 	
 	/**
 	 * Gibt die Informationen eines Buches aus.
 	 */
-	public String output() {
+	public void output() {
 		System.out.println("Titel: " + titel);
 		System.out.println("ISBN-13: " + isbn);
-		System.out.println("Bestand: " + b);
+		System.out.println("Bestand: " + bestand);
 	}
 }
